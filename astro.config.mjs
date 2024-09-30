b73453cb6288b7cb905cba2d63792f151f6750b2
Carlos Angulo <cangulor@aliaddo.com>
2024-09-30 15:35:21 -0500

    chore: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,9 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  site: config.site.base_url ? config.site.base_url : "https://aliaddo.help",
   base: config.site.base_path ? config.site.base_path : "/",
+  outDir: './docs',
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: {
     service: squooshImageService(),
